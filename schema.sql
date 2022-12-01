b0b5dd3eb8905cad7964fdb8f4794cbe9e8ee2cf
cristian-umana <cristianumana1@gmail.com>
2022-11-30 20:59:26 -0500

    sql update

--- schema.sql
+++ schema.sql
@@ -1,27 +1,27 @@
 CREATE TABLE users (
-                    id INTEGER,
+                    id INTEGER NOT NULL UNIQUE,
                     username TEXT NOT NULL,
                     password text NOT NULL,
-                    PRIMARY KEY(id)
+                    PRIMARY KEY(id AUTOINCREMENT)
                 );
 
 CREATE TABLE post (
-                    post_id INTEGER,
+                    post_id INTEGER NOT NULL UNIQUE,
                     post_author TEXT NOT NULL,
                     post_entry TEXT NOT NULL,
-                    post_time NUMERIC,
+                    post_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                     post_replyability text,
-                    PRIMARY KEY(post_id),
+                    PRIMARY KEY(post_id AUTOINCREMENT),
                     FOREIGN KEY(post_author) REFERENCES users(id)
                 );
 
 CREATE TABLE reply (
-                    reply_id INTEGER,
+                    reply_id INTEGER NOT NULL UNIQUE,
                     reply_author TEXT NOT NULL,
                     reply_entry TEXT NOT NULL,
-                    reply_time NUMERIC,
+                    reply_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                     post_id INTEGER,
-                    PRIMARY KEY(reply_id),
+                    PRIMARY KEY(reply_id AUTOINCREMENT),
                     FOREIGN KEY(post_id) REFERENCES post(post_id),
                     FOREIGN KEY(reply_author) REFERENCES users(id)
                 );
