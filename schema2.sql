3e264d4714efa2eeed4a36864146550d1b2c2c94
cristian-umana <cristianumana1@gmail.com>
2022-12-01 22:38:56 -0500

    new schema

---
+++ schema2.sql
@@ -0,0 +1,24 @@
+CREATE TABLE post (
+                    post_id INTEGER NOT NULL UNIQUE,
+                    post_entry TEXT NOT NULL,
+                    post_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    post_replyability text,
+                    PRIMARY KEY(post_id AUTOINCREMENT),
+                );
+
+CREATE TABLE reply (
+                    reply_id INTEGER NOT NULL UNIQUE,
+                    post_id INTEGER,
+                    reply_entry TEXT NOT NULL,
+                    reply_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    PRIMARY KEY(reply_id AUTOINCREMENT),
+                    FOREIGN KEY(post_id) REFERENCES post(post_id),
+                );
+
+CREATE TABLE tags (
+                post_id INTEGER,
+                reply_id INTEGER,
+                tag INTEGER,
+                FOREIGN KEY(post_id) REFERENCES post(post_id),
+                FOREIGN KEY(reply_id) REFERENCES reply(reply_id)
+            );
