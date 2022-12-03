6a74de88df4799e46f2e15f5ff5e72c28eef3880
cristian-umana <cristianumana1@gmail.com>
2022-12-03 14:25:51 -0500

    app.py changes

--- app.py
+++ app.py
@@ -88,6 +88,6 @@ def listen_story():
 @app.route("/listen_advice", methods=["GET", "POST"])
 def listen_advice():
 
-    advice = db.execute("SELECT * FROM reply ORDER BY reply_time")
+    advice = db.execute("SELECT reply_id, reply_time, tag FROM reply JOIN tags ON reply.reply_id = tags.reply_id ORDER BY reply_time")
 
     return render_template("listen_advice.html", advice=advice)
